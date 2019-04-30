31744b5a012485bb37c651ece66d6a05c2d23c4f
boyacao <bycao96@uw.edu>
2019-04-30 14:15:44 -0700

    Activity and xml setup

---
+++ app/src/main/java/edu/washington/bycao96/quizdroid/MainActivity.kt
@@ -0,0 +1,40 @@
+package edu.washington.bycao96.quizdroid
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mathButton = findViewById<View>(R.id.buttonMath)
+        mathButton.setOnClickListener(){
+            val mathIntent = Intent(this@MainActivity, TopicOverviewActivity::class.java )
+            startActivity(mathIntent)
+        }
+
+
+        val codeButton = findViewById<View>(R.id.buttonProgramming)
+        codeButton.setOnClickListener(){
+            val codeIntent = Intent(this@MainActivity,TopicOverviewActivity::class.java)
+            startActivity(codeIntent)
+        }
+
+        val goTButton = findViewById<View>(R.id.buttonGoT)
+        goTButton.setOnClickListener(){
+            val goTIntent = Intent(this@MainActivity, TopicOverviewActivity::class.java)
+            startActivity(goTIntent)
+        }
+
+        val marvelButton = findViewById<View>(R.id.buttonMarvel)
+        marvelButton.setOnClickListener(){
+            val marvelIntent = Intent(this@MainActivity,TopicOverviewActivity::class.java)
+            startActivity(marvelIntent)
+        }
+
+    }
+}
---
+++ app/src/main/java/edu/washington/bycao96/quizdroid/TopicOverviewActivity.kt
@@ -0,0 +1,35 @@
+package edu.washington.bycao96.quizdroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.json.JSONObject
+
+
+class TopicOverviewActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topicoverview)
+    }
+
+    val quizData : JSONObject = JSONObject("""
+        "Math":{
+            "Description" : " This is a Math quiz that helps measure your basic Math knowledge"
+            "NumberOfQuestions" : "4"
+        },
+        "OO Programming":{
+            "Description" : " This is a Object-Oriented quiz that measures your knowledge about OO Programming"
+            "NumberOfQuestions" : "5"
+        },
+        "Marvel":{
+            "Description" : " How much do you know about Marvel hero?"
+            "NumberOfQuestions" : "4"
+        },
+        "Game of Throne":{
+            "Description" : " Are you a real GoT fan?"
+            "NumberOfQuestions" : "4"
+        }
+    """.trimIndent())
+
+
+
+}
