7d02667715832a6366c15c30545c8c6dab1f50ce
boyacao <bycao96@uw.edu>
2019-05-10 14:03:14 -0700

    Fixing bugs

--- app/src/main/java/edu/washington/bycao96/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/bycao96/quizdroid/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.washington.bycao96.quizdroid
 
 import android.content.Intent
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 
